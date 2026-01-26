1f57d83ecb13ef20405eed489a8434636eecae30
Petr Sykora <petr.sykora@gmail.com>
2026-01-26 16:48:35 +0100

    fix: actually configure guess-indent

--- init.lua
+++ init.lua
@@ -249,12 +249,18 @@ rtp:prepend(lazypath)
 -- NOTE: Here is where you install your plugins.
 require('lazy').setup({
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
-  'NMAC427/guess-indent.nvim', -- Detect tabstop and shiftwidth automatically
   -- 'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
   -- 'phha/zenburn.nvim', -- not great
   -- 'tomasiser/vim-code-dark',
   -- 'ribru17/bamboo.nvim',
-  'NMAC427/guess-indent.nvim', -- Detect tabstop and shiftwidth automatically
+  {
+    'NMAC427/guess-indent.nvim', -- Detect tabstop and shiftwidth automatically
+    config = function()
+       require("guess-indent").setup({
+        override_editorconfig = false,
+      })
+    end
+  },
 
   -- NOTE: Plugins can also be added by using a table,
   -- with the first argument being the link and the following
